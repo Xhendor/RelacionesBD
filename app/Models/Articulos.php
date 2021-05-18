3f2614a755ab4eb994cbba547709bf0b6eeb10be
Rosendo Rafal Sosa Canales <rosendorsc@gmail.com>
2021-05-18 09:23:05 -0700

    Se lleno los modelos de Art/Depto/Fabricante

--- app/Models/Articulos.php
+++ app/Models/Articulos.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Articulos extends Model
 {
     use HasFactory;
+
+    protected $fillable=['nombre','precio','marca'];
+
+
+
+    public  function fabricante(){
+
+        return $this->hasOne(Fabricantes::class);
+    }
+    public  function departamento(){
+
+        return $this->hasOne(Departamento::class);
+    }
 }
--- app/Models/Fabricantes.php
+++ app/Models/Fabricantes.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fabricantes extends Model
 {
     use HasFactory;
+
+    protected $fillable=['nombre'];
+
 }
